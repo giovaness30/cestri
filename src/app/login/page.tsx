06d90c628f0f7c1a35940613cf80bf742312594e
Giovane Sedano <giovaness30@gmail.com>
2026-03-12 12:58:55 -0300

    fix supabase

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,24 @@
 'use client'
 
 import { useState } from 'react'
-import { createClient } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
 
 export default function Login() {
-  const supabase = createClient()
+  // const supabase = createClient()
   const router = useRouter()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const handleLogin = async () => {
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
+    // const { error } = await supabase.auth.signInWithPassword({
+    //   email,
+    //   password,
+    // })
 
-    if (error) {
-      alert(error.message)
-      return
-    }
+    //  if (error) {
+    //   alert(error.message)
+    //   return
+    // }
 
     router.push('/dashboard')
   }
